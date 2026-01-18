92abda0b6d72587201149a92699d479a7461cd75
bhanu-3333 <bhanuu339@gmail.com>
2026-01-18 18:54:34 +0530

    fix(navigation): standardize default exports for all stack screens

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AppNavigator } from './src/navigation/AppNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Import screens
+import HomeScreen from "./src/screens/HomeScreen";
+import { LoginScreen } from "./src/screens/LoginScreen";
+
+import SignupScreen from "./src/screens/SignupScreen";
+import VerificationScreen from "./src/screens/VerificationScreen";
+import LocationScreen from "./src/screens/LocationScreen";
+import HomePageScreen from "./src/screens/HomePageScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <GestureHandlerRootView className="flex-1">
-      <AppNavigator />
-      <StatusBar style="dark" />
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Verification" component={VerificationScreen} />
+        <Stack.Screen name="Location" component={LocationScreen} />
+        <Stack.Screen name="HomePage" component={HomePageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
