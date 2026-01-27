d7ce34b95410a5edd5c831744bf97079374cd4bb
sakthikumar1311 <sakthi@13112005>
2026-01-27 13:48:33 +0530

    : fix React Navigation error caused by named vs default exports

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AppNavigator } from './src/navigation/AppNavigator';
-
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-      <AppNavigator />
->>>>>>> 05a093002d982871dc00804b80ed5bd6f990be1f
-      <StatusBar style="dark" />
-    </GestureHandlerRootView>
-    </SafeAreaProvider>
-  );
-}
-=======
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation/AppNavigator';
+
 
 export default function App() {
   return (
@@ -26,21 +12,6 @@ export default function App() {
         <AppNavigator />
         <StatusBar style="dark" />
       </GestureHandlerRootView>
-    </SafeAreaProvider>
-  );
-}
-=======
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AppNavigator } from './src/navigation/AppNavigator';
-
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-      <AppNavigator />
->>>>>>> 05a093002d982871dc00804b80ed5bd6f990be1f
-      <StatusBar style="dark" />
-    </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
